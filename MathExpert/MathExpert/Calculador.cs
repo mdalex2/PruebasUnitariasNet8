b628b7e6a59de9e79c5c2b484e4a1ae83e15e4fa
Alexi Mendoza <mdalex2@gmail.com>
2023-11-20 19:31:41 -0300

    Assert rangos impares (lista int)

--- MathExpert/MathExpert/Calculador.cs
+++ MathExpert/MathExpert/Calculador.cs
@@ -2,6 +2,7 @@
 {
     public class Calculador
     {
+        public List<int> listaRandom = new();
         public int Sumar(int n1,int n2)
         {
             return n1 + n2;
@@ -15,6 +16,19 @@
         public double SumarNrosDoubles(double n1, double n2)
         {
             return n1 + n2;
+        }
+
+        public List<int> ObtenerRangoImpares(int inicio, int final)
+        {
+            listaRandom.Clear();
+            for (int i = inicio; i <= final; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    listaRandom.Add(i);
+                }
+            }
+            return listaRandom;
         }
     }
 }
