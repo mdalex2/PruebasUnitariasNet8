e5d6c8f697d1f84678033f20eaf27fa9241d3052
Alexi Mendoza <mdalex2@gmail.com>
2023-11-21 19:08:13 -0300

    Prueba con multiples escenarios.

---
+++ MathExpert/MathExpert/CalculadorGrado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpert
+{
+    public class CalculadorGrado
+    {
+        public int Puntaje { get; set; }
+        public int PorcentajeAsistencia { get; set; }
+
+        public string ObtenerGrado()
+        {
+            string grado = (Puntaje > 90 && PorcentajeAsistencia > 70) ? "A" :
+                (Puntaje > 80 && PorcentajeAsistencia > 60) ? "B" :
+                (Puntaje > 60 && PorcentajeAsistencia > 60) ? "C" :
+                "F";
+            return grado;
+        }
+    }
+}
---
+++ MathExpert/MathExpertNUnitTest/CalculadorGradoNUnitTest.cs
@@ -0,0 +1,97 @@
+﻿using MathExpert;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpertNUnitTest
+{
+    [TestFixture]
+    public class CalculadorGradoNUnitTest
+    {
+        private CalculadorGrado calculadorGrado;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculadorGrado = new CalculadorGrado();
+        }
+
+
+        #region MultiplesEscenarios de prueba
+
+
+        [Test]
+        public void CalculadorGrado_IngresarPuntaje95Asistencia90_ObtenerGradoA()
+        {
+            calculadorGrado.Puntaje = 95;
+            calculadorGrado.PorcentajeAsistencia = 90;
+            string resultado = calculadorGrado.ObtenerGrado();
+
+            Assert.That(resultado, Is.EqualTo("A"));
+        }
+        
+        [Test]
+        public void CalculadorGrado_IngresarPuntaje85Asistencia90_ObtenerGradoB()
+        {
+            calculadorGrado.Puntaje = 85;
+            calculadorGrado.PorcentajeAsistencia = 90;
+            string resultado = calculadorGrado.ObtenerGrado();
+
+            Assert.That(resultado, Is.EqualTo("B"));
+        }
+        
+        [Test]
+        public void CalculadorGrado_IngresarPuntaje65Asistencia90_ObtenerGradoC()
+        {
+            calculadorGrado.Puntaje = 65;
+            calculadorGrado.PorcentajeAsistencia = 90;
+            string resultado = calculadorGrado.ObtenerGrado();
+
+            Assert.That(resultado, Is.EqualTo("C"));
+        }
+        
+        [Test]
+        public void CalculadorGrado_IngresarPuntaje55Asistencia90_ObtenerGradoF()
+        {
+            calculadorGrado.Puntaje = 55;
+            calculadorGrado.PorcentajeAsistencia = 90;
+            string resultado = calculadorGrado.ObtenerGrado();
+
+            Assert.That(resultado, Is.EqualTo("F"));
+        }
+        
+        [Test]
+        [TestCase(95,55)]
+        [TestCase(65,55)]
+        [TestCase(50,90)]
+        public void CalculadorGrado_IngresarPuntaje55Asistencia90_ObtenerGradoFCase(int puntaje, int porcentajeAsistencia)
+        {
+            calculadorGrado.Puntaje = puntaje;
+            calculadorGrado.PorcentajeAsistencia = porcentajeAsistencia;
+            string resultado = calculadorGrado.ObtenerGrado();
+
+            Assert.That(resultado, Is.EqualTo("F"));
+        }
+        
+        [Test]
+        [TestCase(95,55,ExpectedResult = "F")]
+        [TestCase(65,55,ExpectedResult = "F")]
+        [TestCase(50,90,ExpectedResult = "F")]
+        [TestCase(85,90,ExpectedResult = "B")]
+        [TestCase(65,90,ExpectedResult = "C")]
+        [TestCase(95,90,ExpectedResult = "A")]
+        public string CalculadorGrado_IngresarPuntaje55Asistencia90_ObtenerGrado(int puntaje, int porcentajeAsistencia)
+        {
+            calculadorGrado.Puntaje = puntaje;
+            calculadorGrado.PorcentajeAsistencia = porcentajeAsistencia;
+            string resultado = calculadorGrado.ObtenerGrado();
+            return resultado;
+        }
+
+
+        #endregion MultiplesEscenarios de prueba
+    }
+}
